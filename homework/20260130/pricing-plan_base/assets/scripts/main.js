747d37e4d816c421db20cbd8ca278aab98a45b96
jyeonleee <juyeonlee0203@gmail.com>
2026-02-01 19:16:09 +0900

    feat: [작업] 스크립트 리팩토링

--- homework/20260130/pricing-plan_base/assets/scripts/main.js
+++ homework/20260130/pricing-plan_base/assets/scripts/main.js
@@ -3,7 +3,8 @@ const priceSection = body.querySelector('.pricing-section')
 const planCards = priceSection.querySelectorAll('.plan-card')
 const selectInfo = body.querySelector('.selection-info')
 
-planCards.forEach((cardBtn) => {
+// 첫번째 작업
+/* planCards.forEach((cardBtn) => {
   cardBtn.addEventListener('click', (e) => {
     const beforeSelected = priceSection.querySelector('.plan-card.selected')
     const selectCard = e.currentTarget
@@ -23,4 +24,30 @@ planCards.forEach((cardBtn) => {
       플랜(<span>${planPrice}</span>)을 선택하셨습니다.
     `;
   })
+}) */
+
+// 두번째 작업 - 리팩토링
+planCards.forEach((cardBtn) => {
+  cardBtn.addEventListener('click', handleClick);
 })
+
+function handleClick(e) {
+  const beforeSelected = priceSection.querySelector('.plan-card.selected')
+  const selectCard = e.currentTarget
+
+  if (selectCard.classList.contains('selected')) return
+  
+  if (beforeSelected) {
+    beforeSelected.classList.remove('selected')
+  }
+  selectCard.classList.add('selected')
+  selectInfo.classList.add('show')
+
+  const planName = selectCard.dataset.plan
+  const planPrice = selectCard.dataset.price
+
+  selectInfo.innerHTML = `
+    <strong class="highlight">${planName}</strong>
+    플랜(<span>${planPrice}</span>)을 선택하셨습니다.
+  `
+}
