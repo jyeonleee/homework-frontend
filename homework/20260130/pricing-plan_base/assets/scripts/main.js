e636b6d442a5006cb6617022b03bd18cb3afc02f
jyeonleee <juyeonlee0203@gmail.com>
2026-02-01 16:16:46 +0900

    feat: [작업] 스크립트 작업

---
+++ homework/20260130/pricing-plan_base/assets/scripts/main.js
@@ -0,0 +1,26 @@
+const body = document.querySelector('body')
+const priceSection = body.querySelector('.pricing-section')
+const planCards = priceSection.querySelectorAll('.plan-card')
+const selectInfo = body.querySelector('.selection-info')
+
+planCards.forEach((cardBtn) => {
+  cardBtn.addEventListener('click', (e) => {
+    const beforeSelected = priceSection.querySelector('.plan-card.selected')
+    const selectCard = e.currentTarget
+
+    if (beforeSelected) {
+      beforeSelected.classList.remove('selected')
+    }
+
+    selectCard.classList.add('selected')
+    selectInfo.classList.add('show')
+
+    const planName = selectCard.dataset.plan;
+    const planPrice = selectCard.dataset.price;
+
+    selectInfo.innerHTML = `
+      <strong class="highlight">${planName}</strong>
+      플랜(<span>${planPrice}</span>)을 선택하셨습니다.
+    `;
+  })
+})
